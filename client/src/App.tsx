89ddda21ba2ac142cf6ea7967b3e5a777b8d77bf
Julius Lee <7julius.lee@gmail.com>
2021-08-15 19:18:26 -0700

    changed font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Board from './components/Board';
 import Header from './components/Header';
 import Modal from './components/Modal';
