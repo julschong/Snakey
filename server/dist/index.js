09887a36aafd0328db45b35de383e21f5100f8ee
Julius Lee <7julius.lee@gmail.com>
2021-08-15 20:50:55 -0700

    set only save top 10 scores from json

--- server/dist/index.js
+++ server/dist/index.js
@@ -10,25 +10,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
+var fs_1 = __importDefault(require("fs"));
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({});
 var app = express_1.default();
 app.use(cors_1.default({ origin: '*' }));
 app.use(express_1.default.json());
-var user = [];
+var userJson;
+try {
+    userJson = JSON.parse(fs_1.default.readFileSync('./data/ranking.json', {
+        encoding: 'utf8'
+    }));
+}
+catch (err) {
+    fs_1.default.writeFileSync('./data/ranking.json', JSON.stringify({ ranks: [] }, null, 2));
+    userJson = { ranks: [] };
+}
 app.get('/score/top10', function (req, res) {
     res.status(200).json({
         sucess: true,
-        data: __spreadArray([], user).sort(function (a, b) { return b.score - a.score; }).slice(0, 10)
+        data: __spreadArray([], userJson.ranks).sort(function (a, b) { return b.score - a.score; })
+            .slice(0, 10)
     });
 });
 app.post('/score/submit', function (req, res) {
     var _a = req.body, score = _a.score, name = _a.name;
     var newScore = { name: name, score: score };
-    user.push(newScore);
+    userJson.ranks.push(newScore);
+    userJson.ranks.sort(function (a, b) { return b.score - a.score; });
+    userJson.ranks.splice(10);
+    fs_1.default.writeFileSync('./data/ranking.json', JSON.stringify(userJson, null, 2));
     res.status(200).json({
         sucess: true,
-        data: __spreadArray([], user).sort(function (a, b) { return b.score - a.score; }).slice(0, 10)
+        data: __spreadArray([], userJson.ranks).sort(function (a, b) { return b.score - a.score; })
+            .slice(0, 10)
     });
 });
 var port = process.env.PORT || 3003;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 
 import dotenv from 'dotenv';
 
@@ -13,23 +14,45 @@ type Score = {
     score: number;
 };
 
-const user: Score[] = [];
+let userJson: { ranks: [{ name: string; score: number }?] };
+try {
+    userJson = JSON.parse(
+        fs.readFileSync('./data/ranking.json', {
+            encoding: 'utf8'
+        })
+    );
+} catch (err) {
+    fs.writeFileSync(
+        './data/ranking.json',
+        JSON.stringify({ ranks: [] }, null, 2)
+    );
+    userJson = { ranks: [] };
+}
 
 app.get('/score/top10', (req, res) => {
     res.status(200).json({
         sucess: true,
-        data: [...user].sort((a, b) => b.score - a.score).slice(0, 10)
+        data: [...userJson.ranks]
+            .sort((a, b) => b!.score - a!.score)
+            .slice(0, 10)
     });
 });
 
 app.post('/score/submit', (req, res) => {
     const { score, name }: Score = req.body;
     const newScore = { name, score };
-    user.push(newScore);
+    userJson.ranks.push(newScore);
+    userJson.ranks.sort((a, b) => b!.score - a!.score);
+
+    userJson.ranks.splice(10);
+
+    fs.writeFileSync('./data/ranking.json', JSON.stringify(userJson, null, 2));
 
     res.status(200).json({
         sucess: true,
-        data: [...user].sort((a, b) => b.score - a.score).slice(0, 10)
+        data: [...userJson.ranks]
+            .sort((a, b) => b!.score - a!.score)
+            .slice(0, 10)
     });
 });
 
